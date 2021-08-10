b06005ec1c8970c816b82876acc75f0ef970ce0b
Maxim Evlampiev <max1361@yandex.ru>
2021-08-10 12:35:36 +0300

    first commit

---
+++ user.js
@@ -0,0 +1,16 @@
+const user = {
+    name: 'Elena',
+    age: 25
+}
+
+const user2 = {
+    name: 'Igor',
+    age: 17
+}
+
+module.exports = {
+    user: user,
+    sayhello() {
+        console.log('Hello!');
+    }
+}
