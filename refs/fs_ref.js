93194176b27f26fb8cfdc82b2a1814cfe30f6278
Maxim Evlampiev <max1361@yandex.ru>
2021-08-10 13:44:25 +0300

    3Commit

--- refs/fs_ref.js
+++ refs/fs_ref.js
@@ -1,3 +1,35 @@
 const fs = require('fs');
+const path = require('path')
+    /*
+    fs.mkdir(path.join(__dirname, 'notes'), (err) => {
+        if (err) { throw new Error(err) }
 
-fs.mkdir
+        console.log("Folder is Created!")
+    })
+
+    */
+
+const newLocal = fs.writeFile(path.join(__dirname, 'notes', 'mynotes.txt'),
+    'Hello World',
+    (err) => {
+        if (err) { throw err; }
+        console.log("File was Created!");
+
+        fs.appendFile(path.join(__dirname, 'notes', 'mynotes.txt'),
+            'From appendFile',
+            (err) => {
+                if (err) { throw err; }
+                console.log("File was Changed!");
+            }
+        );
+    }
+);
+
+fs.readFile(
+    path.join(__dirname, 'notes', 'mynotes.txt'),
+    'utf-8',
+    (err, data) => {
+        if (err) { throw err; }
+        console.log(data);
+    }
+)
