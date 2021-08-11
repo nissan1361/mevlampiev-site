638b60674aae1aa3e51722e58e77b2ad8d7d56c5
Maxim Evlampiev <max1361@yandex.ru>
2021-08-11 14:06:58 +0300

    commit 11 08 2021

---
+++ refs/os_ref.js
@@ -0,0 +1,22 @@
+const os = require('os');
+
+// Platform
+console.log(os.platform());
+
+// Architecture
+console.log(os.arch())
+
+// Processor
+console.log(os.cpus())
+
+// Free RAM
+console.log(os.freemem())
+
+// All RAM
+console.log(os.totalmem())
+
+// Root dir
+console.log(os.homedir())
+
+// System work time
+console.log(os.uptime())
